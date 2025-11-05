50d830506e5c1f57ee95c9cbf62c481e96fefd4a
Gkmaster-coder <gouravkashyap2468@gmail.com>
2025-11-05 15:02:03 +0530

    create Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import BookingPage from './pages/BookingPage'
+import BookingPage from './pages/BookingPage';
+import AdminDashboard from './pages/AdminDashboard';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import CompleteCaseForm from './components/caseForm/CompleteCaseForm';
+
 
 const App = () => {
   return (
     <>
-    <BookingPage/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<BookingPage/>}/>
+        <Route path='/admin-dashboard' element={<AdminDashboard/>} />
+      </Routes>
+ <CompleteCaseForm/>
+    </Router>
     </>
   )
 }
